685cb0edee471b1332b942778f39ee98bbf33238
lbutlr093 <lbutlr093@gmail.com>
2017-07-26 21:00:35 -0500

    basic functionality

--- lyrics_parser.py
+++ lyrics_parser.py
@@ -1,8 +1,8 @@
 ## TODO: Description
 
-# +gather all files in folder
-# +open only files > _ bits/bytes
-# +trim out beginning or ending white space
+# gather all files in folder
+# open only files > _ bits/bytes
+# trim out beginning or ending white space
 # find and remove sections with other artists in lyrics
 # keep sections with descriptive titles (Verse, Chorus, etc...)
 # re-write lyrics to file
@@ -12,11 +12,32 @@ import re
 import fnmatch
 
 
-artist = 'wutangclan'			# testing only
-file_path = ("artists/" + str(artist) + "/")
-#print(file_path)
-file_list = fnmatch.filter(os.listdir(file_path), '*.txt')
-#print(file_list)
+artist = 'nas'											# Testing only
+
+## TODO: below
+#file_path = ("artists/" + str(artist) + "/")
+#file_list = fnmatch.filter(os.listdir(file_path), '*.txt')
+
+# Change to input file name eventually
+output_file = open('test_output.txt', 'w')
+with open('artists/nas/nasislike.txt') as f:
+	flag = False
+	# Look for artist
+	for line in f:
+		# Look for brackets [ ]
+		if (re.search(r"[[\]]+", line)) != None:		# Header
+			if artist.lower() in line.lower():
+				output_file.write(line)
+				flag = True
+			else:
+				flag = False
+		else:											# Not a header
+			if flag == True:
+				output_file.write(line)
+
+
+
+'''
 artists = raw_input("artists to search for in " + str(artist) + ": ")
 artists = artists.lower().replace(" ", "").split(",")
 file_path = file_path + "individual_artists/"
@@ -26,20 +47,4 @@ except OSError, e:
 	if e.errno != 17:
 		raise
 	pass
-
-## Create a file for the lyrics of each artist
-artist_pos = 0
-while artist_pos < len(artists):
-	file = open(str(file_path) + str(artists[artist_pos]) + '.txt', 'w')
-	artist_pos += 1
-
-
-
-song_pos = 0
-'''while song_pos < len(file_list):
-	with open(file_list[song_pos]) as :
-
-
-
-	song_pos += 1
 '''
